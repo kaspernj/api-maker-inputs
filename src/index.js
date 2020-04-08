71d7d23f3b4f032a69687fe10e91e2cb645e4e90
kaspernj <kaspernj@gmail.com>
2020-04-08 14:23:34 +0200

    Use require instead of import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import Checkbox from "./checkbox"
-import Input from "./input"
-import Money from "./money"
-import Select from "./select"
+const Checkbox = require("./checkbox").default
+const Input = require("./input").default
+const Money = require("./money").default
+const Select = require("./select").default
 
 module.exports = {
   Checkbox,
