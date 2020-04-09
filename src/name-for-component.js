557b384c95f57fbdcfc95104c9cfdb56ead607bd
kaspernj <kaspernj@gmail.com>
2020-04-09 04:52:34 +0200

    Fixed given name not used

--- src/name-for-component.js
+++ src/name-for-component.js
@@ -1,7 +1,7 @@
 const inflection = require("inflection")
 
 export default function ApiMakerNameForComponent(component) {
-  if (name in component.props) {
+  if ("name" in component.props) {
     return component.props.name
   } else if (component.props.attribute && component.props.model) {
     return `${component.props.model.modelClassData().paramKey}[${inflection.underscore(component.props.attribute)}]`
